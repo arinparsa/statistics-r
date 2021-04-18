1afb2e6faa97106f786eb10e7e3ad5c0c1c90b18
arinparsa <gzillarules@gmail.com>
2021-04-18 16:34:11 -0700

    Learning linear regression, hypothesis testing, bayes theorem and probability.

---
+++ scripts/Hypothesis Testing.R
@@ -0,0 +1,22 @@
+# Author:Arin Parsa
+# Date: 03/22/2021
+
+# Null hypothesis: There is no correlation between the petal length of a flower and the species
+# Alternate hypothesis: There is a correlation between the petal length of a flower and the species
+
+#Start by investigating the difference in means
+summary(iris$Petal.Length)
+iris$Species = as.numeric(iris$Species)
+iris$Petal.Length.Class = ifelse(iris$Petal.Length < median(iris$Petal.Length), "Low", "High")
+table(iris$Petal.Length.Class)
+t.test(iris$Species[iris$Petal.Length.Class=="Low"], iris$Species[iris$Petal.Length.Class=="High"], na.rm = TRUE)
+
+model <- lm(Species ~ Petal.Length, data = iris)
+summary(model)
+
+# t-test with cereal data
+library(MASS)
+
+data("UScereal")
+
+t.test(UScereal$calories[UScereal$mfr=="K"], UScereal$calories[UScereal$mfr=="G"], na.rm = TRUE)
